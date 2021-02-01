e14c5b7385c0b35d25643ad7a0aad1a31e0b8085
Madeline Cameron <maddie@qnzl.co>
2021-02-01 08:40:54 -0500

    refactor: move to reconciling rather than periodic updates

--- index.js
+++ index.js
@@ -1,93 +1,35 @@
 const { spawnSync } = require(`child_process`)
+const { resolve } = require(`path`)
 const hyperdrive = require(`hyperdrive`)
 const hyperswarm = require(`hyperswarm`)
-const cronitor = require(`cronitor-caller`)(process.env.CRONITOR_KEY)
 const moment = require(`moment`)
 const fetch = require(`node-fetch`)
 const debug = require(`debug`)(`qnzl:aw:sync-client`)
+const fs = require(`fs`)
 
-const TEN_MINUTES = 5 * 60 * 1000
-
-const CRONITOR_AW_ID = process.env.CRONITOR_AW_ID
-
-let lastUploadMarker = null
-
-const sliceData = (data) => {
-  const dataSlices = []
-
-  debug(`slice data of len ${data.length}`)
-  for (let i = 0; i < Math.ceil(data.length / 300); i++) {
-    const sliceBegin = i * 300
-    let sliceEnd = (i + 1) * 300
-
-    sliceEnd = sliceEnd > data.length ? data.length : sliceEnd
-
-    debug(`slice begin: ${sliceBegin}\nSlice end: ${sliceEnd}`)
-
-    const dataSlice = data.slice(sliceBegin, sliceEnd)
-    dataSlices.push(dataSlice)
-  }
-
-  return dataSlices
-}
-
-const filterData = (data, marker) => {
-  if (!marker) return data
-
-  return data.filter((event) => {
-    const eventTimestamp = moment(event.timestamp)
-
-    return moment(marker).isBefore(eventTimestamp)
-  })
-}
+const SIX_HOURS = 6 * 60 * 60 * 1000
 
 const heartbeat = async () => {
   debug(`activity watch sync heartbeat`)
-  let localBuckets = await fetch(`http://localhost:5600/api/0/buckets`)
+  const bucketData = await fetch(`http://localhost:5600/api/0/export`)
 
-  localBuckets = await localBuckets.json()
+  const data = await bucketData.json()
 
-  cronitor.run(CRONITOR_AW_ID)
-
-  debug(`got buckets`)
-  const promises = Object.values(localBuckets).map(async ({ id }) => {
-    debug(`get events for bucket ${id}`)
-    let bucketData = await fetch(`http://localhost:5600/api/0/buckets/${id}/events`)
-
-    let data = await bucketData.json()
-    data = filterData(data, lastUploadMarker)
-
-    debug(`got ${data.length} events for ${id}`)
-    let dataSlices = sliceData(data)
-
-    debug(`created ${dataSlices.length} slices to upload`)
-    dataSlices.forEach(async (slice, index) => {
-      debug(`upload slice #${index} for bucket ${id}`)
-      await fetch(`${process.env.AW_SYNC_SERVER}/api/v1/add/${id}`, {
-        method: `POST`,
-        headers: {
-          Authorization: `Bearer ${process.env.AW_AUTHORIZATION_TOKEN}`,
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          data: slice,
-          timestamp: new Date()
-        })
-      })
-    })
+  debug(`reconciling data export with remote`)
+  await fetch(`${process.env.AW_SYNC_SERVER}/api/v1/reconcile`, {
+    method: `POST`,
+    headers: {
+      Authorization: `Bearer ${process.env.AW_AUTHORIZATION_TOKEN}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data),
   })
-
-  Promise.all(promises)
-    .then(() => {
-      lastUploadMarker = moment()
-    })
-
 
   if (process.argv[2] !== `once`) {
     debug(`create next heartbeat timer`)
     setTimeout(() => {
       heartbeat()
-    }, TEN_MINUTES)
+    }, SIX_HOURS)
   } else {
     process.exit(0)
   }
@@ -96,7 +38,6 @@ const heartbeat = async () => {
 process.on(`SIGINT`, () => {
   if (process.argv[2] === `once`) return
 
-  cronitor.fail(CRONITOR_AW_ID)
   const out = spawnSync(`node ${__filename}`, [ `once` ], { encoding: `utf8` })
 
   process.exit(10)
